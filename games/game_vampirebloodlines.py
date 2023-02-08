402e98a8a686fc8a9b7f5985f9ea40bf272f5ba9
John L C <JayLCypher@users.noreply.github.com>
2023-02-08 04:10:22 +0100

    Add files via upload
    Updated and fixed plugin

--- games/game_vampirebloodlines.py
+++ games/game_vampirebloodlines.py
@@ -1,127 +1,110 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import os
-from pathlib import Path
-from typing import List
 
-from PyQt6.QtCore import QDir
+import pathlib
 
+import PyQt6.QtCore as QtCore
 import mobase
 
 from ..basic_game import BasicGame, BasicGameSaveGame
 
-
 class VampireModDataChecker(mobase.ModDataChecker):
-    def __init__(self):
-        super().__init__()
-        self.validDirNames = [
-            "cfg",
-            "cl_dlls",
-            "dlg",
-            "dlls",
-            "maps",
-            "materials",
-            "media",
-            "models",
-            "particles",
-            "python",
-            "resource",
-            "scripts",
-            "sound",
-            "vdata",
-        ]
+	def __init__(self):
+		super().__init__()
+		self.validDirNames = [
+			"cfg",
+			"cl_dlls",
+			"dlg",
+			"dlls",
+			"maps",
+			"materials",
+			"media",
+			"models",
+			"particles",
+			"python",
+			"resource",
+			"SAVE",
+			"scripts",
+			"sound",
+			"vdata",
+		]
 
-    def dataLooksValid(
-        self, tree: mobase.IFileTree
-    ) -> mobase.ModDataChecker.CheckReturn:
-        for entry in tree:
-            if not entry.isDir():
-                continue
-            if entry.name().casefold() in self.validDirNames:
-                return mobase.ModDataChecker.VALID
-        return mobase.ModDataChecker.INVALID
+	def dataLooksValid(self, filetree: mobase.IFileTree) -> mobase.ModDataChecker.CheckReturn:
+		for entry in filetree:
+			if not entry.isDir(): continue
+			if entry.name().casefold() in self.validDirNames: return mobase.ModDataChecker.VALID
+		return mobase.ModDataChecker.INVALID
 
+class VampireModDataContent(mobase.ModDataContent):
+	def __init__(self):
+		super().__init__()
 
 class VampireSaveGame(BasicGameSaveGame):
-    _filepath: Path
-
-    def __init__(self, filepath: Path):
-        super().__init__(filepath)
-        self._filepath = filepath
-        self.name = None
-        self.elapsedTime = None
-
+	def __init__(self, filepath: pathlib.Path):
+		super().__init__(filepath)
 
 class VampireLocalSavegames(mobase.LocalSavegames):
-    def __init__(self, myGameSaveDir):
-        super().__init__()
-        self._savesDir = myGameSaveDir.absolutePath()
+	def __init__(self, myGameSaveDir: QtCore.QDir):
+		super().__init__()
+		self._savesDir = myGameSaveDir.absolutePath()
 
-    def mappings(self, profile_save_dir):
-        m = mobase.Mapping()
-        m.createTarget = True
-        m.isDirectory = True
-        m.source = profile_save_dir.absolutePath()
-        m.destination = self._savesDir
+	def mappings(self, profile_save_dir: QtCore.QDir): return [mobase.Mapping(
+			source=profile_save_dir.absolutePath(),
+			destination=self._savesDir,
+			is_directory=True,
+			create_target=True
+		)]
 
-        return [m]
-
-    def prepareProfile(self, profile):
-        return profile.localSavesEnabled()
-
+	def prepareProfile(self, profile):
+		return profile.localSavesEnabled()
 
 class VampireTheMasqueradeBloodlinesGame(BasicGame):
-    Name = "Vampire - The Masquerade: Bloodlines Support Plugin"
-    Author = "John"
-    Version = "1.0.0"
-    Description = "Adds support for Vampires: The Masquerade - Bloodlines"
+	Name = "Vampire - The Masquerade: Bloodlines Support Plugin"
+	Author = "John"
+	Version = "1.0.0"
+	Description = "Adds Mod Organizer 2 version 2.5.0 Basic Game support for Vampires: The Masquerade - Bloodlines"
 
-    GameName = "Vampire - The Masquerade: Bloodlines"
-    GameShortName = "vampirebloodlines"
-    GameNexusName = "vampirebloodlines"
-    GameNexusId = 437
-    GameSteamId = [2600]
-    GameGogId = [1207659240]
-    GameBinary = "vampire.exe"
-    GameDataPath = "vampire"
-    GameDocumentsDirectory = "%GAME_PATH%/vampire/cfg"
-    GameSavesDirectory = "%GAME_PATH%/vampire/SAVE"
-    GameSaveExtension = "sav"
-    GameSupportURL = (
-        r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
-        "Game:-Vampire:-The-Masquerade-%E2%80%90-Bloodlines"
-    )
+	GameName = "Vampire The Masquerade - Bloodlines"
+	GameShortName = "vampirebloodlines"
+	GameNexusName = "vampirebloodlines"
+	GameNexusId = 437
+	GameSteamId = [2600]
+	GameGogId = [1207659240]
+	GameBinary = "vampire.exe"
+	GameDataPath = "vampire"
+	GameDocumentsDirectory = "%GAME_PATH%/vampire/cfg"
+	GameSavesDirectory = "%GAME_PATH%/vampire/SAVE"
+	GameSaveExtension = "sav"
+	GameSupportURL = (
+		r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
+		"Game:-Vampire:-The-Masquerade-%E2%80%90-Bloodlines"
+	)
 
-    def init(self, organizer: mobase.IOrganizer) -> bool:
-        super().init(organizer)
-        self._featureMap[mobase.ModDataChecker] = VampireModDataChecker()
-        self._featureMap[mobase.SaveGameInfo] = VampireSaveGame(
-            Path(self.savesDirectory().absolutePath())
-        )
-        self._featureMap[mobase.LocalSavegames] = VampireLocalSavegames(
-            self.savesDirectory()
-        )
-        return True
+	GameTrueName = "Vampire: The Masquerade - Bloodlines"
 
-    def initializeProfile(self, path: QDir, settings: mobase.ProfileSetting):
-        # Create .cfg files if they don't exist
-        for iniFile in self.iniFiles():
-            iniPath = self.documentsDirectory().absoluteFilePath(iniFile)
-            if not os.path.exists(iniPath):
-                with open(iniPath, "w") as _:
-                    pass
+	def init(self, organizer: mobase.IOrganizer) -> bool:
+		super().init(organizer)
+		self._featureMap[mobase.ModDataChecker] = VampireModDataChecker()
+		self._featureMap[mobase.ISaveGame] = VampireSaveGame(pathlib.Path(self.savesDirectory().absolutePath()))
+		self._featureMap[mobase.LocalSavegames] = VampireLocalSavegames(self.savesDirectory())
+		return True
 
-        super().initializeProfile(path, settings)
+	def iniFiles(self):
+		return ["autoexec.cfg", "user.cfg"]
 
-    def version(self):
-        # Don't forget to import mobase!
-        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.final)
-
-    def iniFiles(self):
-        return ["autoexec.cfg", "user.cfg"]
-
-    def listSaves(self, folder: QDir) -> List[mobase.ISaveGame]:
-        ext = self._mappings.savegameExtension.get()
-        return [
-            VampireSaveGame(path)
-            for path in Path(folder.absolutePath()).glob(f"*.{ext}")
-        ]
+	def executables(self) -> list[mobase.ExecutableInfo]:
+		execs = []
+		if self.getLauncherName():
+			execs.append(
+				mobase.ExecutableInfo(
+					self.GameTrueName,
+					QtCore.QFileInfo(self.gameDirectory().absoluteFilePath(self.getLauncherName())),
+				)
+			)
+		execs.append(
+			mobase.ExecutableInfo(
+				self.GameTrueName,
+				QtCore.QFileInfo(self.gameDirectory().absoluteFilePath(self.binaryName())),
+			)
+		)
+		return execs
